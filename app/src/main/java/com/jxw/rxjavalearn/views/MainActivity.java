89f0027b6335d2d345571934e9effec74bdf86bb
John Obi <johngorithm@gmail.com>
2019-04-11 16:11:40 +0100

    feat: create observable from single object

--- app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
+++ app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
@@ -5,30 +5,30 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.jxw.rxjavalearn.R;
-import com.jxw.rxjavalearn.data.DataSource;
 import com.jxw.rxjavalearn.model.Task;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Observable<Task> taskObservable;
     private CompositeDisposable disposable = new CompositeDisposable();
+    /* package */ Task task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        taskObservable = getTaskObservable();
+        task = new Task("Take Lunch", false, 5);
 
-        taskObservable.subscribe(new Observer<Task>() {
+        getTaskObservable().subscribe(new Observer<Task>() {
             @Override
             public void onSubscribe(Disposable d) {
                 disposable.add(d);
@@ -54,14 +54,16 @@ public class MainActivity extends AppCompatActivity {
 
     public Observable<Task> getTaskObservable() {
         return Observable
-                .fromIterable(DataSource.createTasks())
-                .subscribeOn(Schedulers.io())
-                .filter(new Predicate<Task>() {
+                .create(new ObservableOnSubscribe<Task>() {
                     @Override
-                    public boolean test(Task task) throws Exception {
-                        return task.isComplete();
+                    public void subscribe(ObservableEmitter<Task> emitter) throws Exception {
+                        if (!emitter.isDisposed()) {
+                            emitter.onNext(task);
+                            emitter.onComplete();
+                        }
                     }
                 })
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
