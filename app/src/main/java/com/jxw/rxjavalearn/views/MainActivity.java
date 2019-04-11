124fdbe8da2f31b71bee0d9fe85046e155d3a65d
John Obi <johngorithm@gmail.com>
2019-04-11 20:00:39 +0100

    feat: create observabe from range and repeat operators

--- app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
+++ app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
@@ -5,20 +5,16 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.jxw.rxjavalearn.R;
-import com.jxw.rxjavalearn.data.DataSource;
-import com.jxw.rxjavalearn.model.Task;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Observable<Task> taskObservable;
     private CompositeDisposable disposable = new CompositeDisposable();
 
     @Override
@@ -26,44 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        taskObservable = getTaskObservable();
-
-        taskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                disposable.add(d);
-                Log.d(TAG, "ON-SUBSCRIBE IS CALLED");
-            }
-
-            @Override
-            public void onNext(Task task) {
-                Log.d(TAG, "onNext: " + task.getDescription());
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "ON-ERROR IS CALLED");
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "ON-COMPLETE IS CALLED");
-            }
-        });
-    }
-
-    public Observable<Task> getTaskObservable() {
-        return Observable
-                .fromIterable(DataSource.createTasks())
+        Observable.range(0, 5)
+                .repeat(2)
                 .subscribeOn(Schedulers.io())
-                .filter(new Predicate<Task>() {
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
                     @Override
-                    public boolean test(Task task) throws Exception {
-                        return task.isComplete();
+                    public void onSubscribe(Disposable d) {
+
                     }
-                })
-                .observeOn(AndroidSchedulers.mainThread());
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.d(TAG, "onNext: "+integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete: is called");
+                    }
+                });
+
     }
+
 
     @Override
     protected void onDestroy() {
