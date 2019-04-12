81df395034e79926c3874824aabf80b43dd8d344
John Obi <johngorithm@gmail.com>
2019-04-12 15:37:13 +0100

    feat: create observable from map transformation operator

--- app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
+++ app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
@@ -1,21 +1,22 @@
 package com.jxw.rxjavalearn.views;
 
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
 
 import com.jxw.rxjavalearn.R;
 import com.jxw.rxjavalearn.data.DataSource;
 import com.jxw.rxjavalearn.model.Task;
 
-import java.util.concurrent.Callable;
-
 import io.reactivex.Observable;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -26,45 +27,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        subscribeToAnObject();
-    }
-
-    public Observable<Task> getTaskObservable() {
-        return Observable
-                .fromCallable(new Callable<Task>() {
-                    @Override
-                    public Task call() throws Exception {
-                        return DataSource.getTask();
-                    }
-                })
+        Observable<String> stringObservable = Observable
+                .fromIterable(DataSource.createTasks())
+                .map(extractDescFunction)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
-    }
 
-    public void subscribeToAnObject() {
-        getTaskObservable().subscribe(new Observer<Task>() {
+        stringObservable.subscribe(new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
                 disposable.add(d);
-                Log.d(TAG, "onSubscribe: is called");
             }
 
             @Override
-            public void onNext(Task task) {
-                Log.d(TAG, "onNext: " + task.getDescription());
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: " + s);
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.e(TAG, "onError: is called", e);
+
             }
 
             @Override
             public void onComplete() {
-                Log.d(TAG, "onComplete: is called");
+
             }
         });
+
     }
+
+
+
+    Function<Task, String> extractDescFunction = new Function<Task, String>() {
+        @Override
+        public String apply(Task task) {
+            return task.getDescription();
+        }
+    };
 
     @Override
     protected void onDestroy() {
