798501c3786aa0884862b125fb3e009054254161
John Obi <johngorithm@gmail.com>
2019-04-12 11:21:08 +0100

    feat: create observable fromArray operator

--- app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
+++ app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.jxw.rxjavalearn.R;
-import com.jxw.rxjavalearn.data.DataSource;
 import com.jxw.rxjavalearn.model.Task;
 
 import io.reactivex.Observable;
@@ -18,17 +17,33 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Observable<Task> taskObservable;
     private CompositeDisposable disposable = new CompositeDisposable();
+    private Task[] list = new Task[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        taskObservable = getTaskObservable();
 
-        taskObservable.subscribe(new Observer<Task>() {
+        list[0] = new Task("Call my baby", true, 3);
+        list[1] = new Task("Walk the dog", false, 2);
+        list[2] = new Task("Make my bed", true, 1);
+        list[3] = new Task("Unload the dishwasher", false, 0);
+        list[4] = new Task("Make dinner", true, 5);
+
+        subscribeToAnObject();
+    }
+
+    public Observable<Task> getTaskObservable() {
+        return Observable
+                .fromArray(list)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public void subscribeToAnObject() {
+        getTaskObservable().subscribe(new Observer<Task>() {
             @Override
             public void onSubscribe(Disposable d) {
                 disposable.add(d);
@@ -50,19 +65,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "ON-COMPLETE IS CALLED");
             }
         });
-    }
-
-    public Observable<Task> getTaskObservable() {
-        return Observable
-                .fromIterable(DataSource.createTasks())
-                .subscribeOn(Schedulers.io())
-                .filter(new Predicate<Task>() {
-                    @Override
-                    public boolean test(Task task) throws Exception {
-                        return task.isComplete();
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread());
     }
 
     @Override
