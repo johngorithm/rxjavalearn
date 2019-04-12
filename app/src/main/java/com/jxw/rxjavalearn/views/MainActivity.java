6da1ca1bb58998abaa095e32645eedfa4460833f
John Obi <johngorithm@gmail.com>
2019-04-12 11:00:10 +0100

    feat: create observable from interval and timer operators

--- app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
+++ app/src/main/java/com/jxw/rxjavalearn/views/MainActivity.java
@@ -6,11 +6,14 @@ import android.util.Log;
 
 import com.jxw.rxjavalearn.R;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,19 +25,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable.range(0, 5)
-                .repeat(2)
+        Observable.interval(1, TimeUnit.SECONDS)
+                .takeWhile(new Predicate<Long>() {
+                    @Override
+                    public boolean test(Long aLong) throws Exception {
+                        return aLong <= 5;
+                    }
+                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Integer>() {
+                .subscribe(new Observer<Long>() {
                     @Override
                     public void onSubscribe(Disposable d) {
 
                     }
 
                     @Override
-                    public void onNext(Integer integer) {
-                        Log.d(TAG, "onNext: "+integer);
+                    public void onNext(Long aLong) {
+                        Log.d(TAG, "onNext: "+ aLong.toString());
                     }
 
                     @Override
@@ -44,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onComplete() {
-                        Log.d(TAG, "onComplete: is called");
+
                     }
                 });
 
